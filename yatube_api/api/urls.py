71616d96715c71b07aea04e95f1fd81a3c82669b
SaveliyKrivov <krivovsaveliy28@gmail.com>
2025-04-21 13:19:48 +0300

    authentication JWT djoser done

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 urlpatterns = [
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
